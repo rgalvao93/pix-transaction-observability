e89ca62973746de2f38b7797e1611a7175705dcd
rgalvao93 <rgalvao93@outlook.com>
2026-02-06 11:56:38 -0300

    atualização de classes

--- transaction-service/src/main/java/com/rodrigogalvao/transaction/TransactionServiceApplication.java
+++ transaction-service/src/main/java/com/rodrigogalvao/transaction/TransactionServiceApplication.java
@@ -1,4 +1,4 @@
-package com.rodrigogalvao.transactionservice;
+package com.rodrigogalvao.transaction;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- transaction-service/src/main/java/com/rodrigogalvao/transaction/controller/TransactionController.java
+++ transaction-service/src/main/java/com/rodrigogalvao/transaction/controller/TransactionController.java
@@ -1,4 +1,4 @@
-package com.rodrigogalvao.transactionservice;
+package com.rodrigogalvao.transaction.controller;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
